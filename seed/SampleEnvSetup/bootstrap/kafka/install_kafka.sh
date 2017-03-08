cd1fb6813292ea92de260969753d7a1f0dfe8506
Volodymyr Ryhel <fashuser@gmail.com>
2017-03-08 11:02:58 +0200

    Migrate kafka scripts to init.d

--- seed/SampleEnvSetup/bootstrap/kafka/install_kafka.sh
+++ seed/SampleEnvSetup/bootstrap/kafka/install_kafka.sh
@@ -1,9 +1,18 @@
 cd /opt
-wget -q http://apache.ip-connect.vn.ua/kafka/0.10.2.0/kafka_2.12-0.10.2.0.tgz
-tar -xf kafka_2.12-0.10.2.0.tgz
-rm kafka_2.12-0.10.2.0.tgz
-mv /opt/kafka_2.12-0.10.2.0 /opt/kafka
+wget -q http://apache.ip-connect.vn.ua/kafka/0.10.2.0/kafka_$1.tgz
+tar -xf kafka_$1.tgz
+rm kafka_$1.tgz
+mv /opt/kafka_$1 /opt/kafka
+
 cd /opt/kafka
+
 mkdir logs
+
+cd logs
 touch zoo-server.log
 touch kafka-server.log
+
+cd /opt/kafka/config
+
+echo "advertised.host.name=$2" >> server.properties
+echo "advertised.port=$3" >> server.properties
---
+++ seed/SampleEnvSetup/bootstrap/kafka/prepare_shutdown_symlinks.sh
@@ -0,0 +1,5 @@
+ln -s /etc/init.d/kafka /etc/rc0.d/K62kafka
+ln -s /etc/init.d/kafka /etc/rc6.d/K62kafka
+
+ln -s /etc/init.d/zookeeper /etc/rc6.d/K80zookeeper
+ln -s /etc/init.d/zookeeper /etc/rc0.d/K80zookeeper
