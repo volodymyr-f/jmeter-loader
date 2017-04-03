c28a76583c0a8175f8a3ee6d7f0e6decbeb3786c
Volodymyr Ryhel <fashuser@gmail.com>
2017-04-03 09:27:12 +0300

    Box provisioning: Spark and Cassandra initial commit (Part 3)

--- seed/SampleEnvSetup/bootstrap/cassandra/install_cassandra_properties.sh
+++ seed/SampleEnvSetup/bootstrap/cassandra/install_cassandra_properties.sh
@@ -1,7 +1,8 @@
 cd /etc/cassandra/conf
 
+sed -i "s/start_rpc: false/start_rpc: true/g" cassandra.yaml
 sed -i "s/rpc_address: localhost/rpc_address: 0.0.0.0/g" cassandra.yaml
-sed -i "s/# broadcast_address: 1.2.3.4/broadcast_address: $1/g" cassandra.yaml
+sed -i "s/# broadcast_rpc_address: 1.2.3.4/broadcast_rpc_address: $1/g" cassandra.yaml
 sed -i "s/127.0.0.1/$1/g" cassandra.yaml
 sed -i "s/listen_address: localhost/listen_address: $1/g" cassandra.yaml
 sed -i "s/# broadcast_address: 1.2.3.4/broadcast_address: $1/g" cassandra.yaml
--- seed/SampleEnvSetup/bootstrap/spark/install_spark_consumer.sh
+++ seed/SampleEnvSetup/bootstrap/spark/install_spark_consumer.sh
@@ -6,16 +6,16 @@ cd logs
 touch spark-consumer.log
 
 cd /opt/spark-structured-streaming-loader/src/main/scala/com/loader/
-# Set up kafka host
-sed -i "s/localhost/$1/g" KafkaConsumer.scala
-# Set up kafka topic
-sed -i "s/loader_1/$2/g" KafkaConsumer.scala
 # Set up cassandra host
-sed -i "s/192.168.33.10/$3/g" KafkaConsumer.scala
+sed -i "s/192.168.33.10/$1/g" KafkaConsumer.scala
 # Set up cassandra keyspace
-sed -i "s/into loader/into $4/g" KafkaConsumer.scala
+sed -i "s/into loader/into $2/g" KafkaConsumer.scala
 # Set up cassandra table
-sed -i "s/events_raw/$5/g" KafkaConsumer.scala
+sed -i "s/events_raw/$3/g" KafkaConsumer.scala
+# Set up kafka host
+sed -i "s/localhost/$4/g" KafkaConsumer.scala
+# Set up kafka topic
+sed -i "s/loader_1/$5/g" KafkaConsumer.scala
 cd /opt/spark-structured-streaming-loader
 
 sbt assembly
