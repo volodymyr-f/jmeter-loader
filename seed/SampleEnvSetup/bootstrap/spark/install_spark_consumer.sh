33fc38cafe0fb4e53ac5758e811e7ddde2c3309b
Volodymyr Ryhel <vryhel@cisco.com>
2017-05-05 19:17:11 +0300

    Replace standalone spark cluster with mesos cluster(Part 1)

--- seed/SampleEnvSetup/bootstrap/spark/install_spark_consumer.sh
+++ seed/SampleEnvSetup/bootstrap/spark/install_spark_consumer.sh
@@ -7,15 +7,17 @@ touch spark-consumer.log
 
 cd /opt/spark-structured-streaming-loader/src/main/scala/com/loader/
 # Set up cassandra host
-sed -i "s/192.168.33.10/$1/g" KafkaConsumer.scala
+sed -i "s/192.168.33.11/$1/g" KafkaConsumer.scala
 # Set up cassandra keyspace
 sed -i "s/into loader/into $2/g" KafkaConsumer.scala
 # Set up cassandra table
-sed -i "s/events_raw/$3/g" KafkaConsumer.scala
+sed -i "s/page_count_hourly/$3/g" KafkaConsumer.scala
 # Set up kafka host
-sed -i "s/localhost/$4/g" KafkaConsumer.scala
+sed -i "s/192.168.33.10/$4/g" KafkaConsumer.scala
 # Set up kafka topic
 sed -i "s/loader_1/$5/g" KafkaConsumer.scala
+# Set up mesos socket
+sed -i "s/local\[2\]/$6/g" KafkaConsumer.scala
 cd /opt/spark-structured-streaming-loader
 
 sbt assembly
